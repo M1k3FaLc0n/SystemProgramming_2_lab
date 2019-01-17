db0c5a7bd19ceaaa94e11a891a320013cc2c6a8f
MIchael <20vampir11@gmail.com>
2019-01-18 00:42:51 +0300

    open file as BufferedImage

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,20 +1,25 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import javax.swing.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -22,23 +27,35 @@ public class Main extends Application {
     private Button btnDoCode = null;
     private Button btnSaveFile = null;
 
-    private FileChooser fileChooser = null;
+    private HBox horizBox = null;
+    private VBox vertBox = null;
 
+    private FileChooser fileChooser = null;
+    private File selectedFile = null;
+
+    private BufferedImage image = null;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
 //        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         StackPane root = new StackPane();
 
-        HBox horizBox = new HBox();
-        VBox vertBox = new VBox();
+        horizBox = new HBox();
+        vertBox = new VBox();
+
+        vertBox.setSpacing(10);
+        horizBox.setSpacing(50);
+        horizBox.setPadding(new Insets(100,50,100,50));
 
         root.getChildren().add(horizBox);
         horizBox.getChildren().add(vertBox);
 
         btnOpenFile = new Button("Open File");
+        btnOpenFile.setMinWidth(100);
         btnDoCode = new Button("Do Code");
+        btnDoCode.setMinWidth(100);
         btnSaveFile = new Button("Save File");
+        btnSaveFile.setMinWidth(100);
 
         vertBox.getChildren().add(btnOpenFile);
         vertBox.getChildren().add(btnDoCode);
@@ -47,8 +64,8 @@ public class Main extends Application {
         fileChooser = new FileChooser();
         addFileChooserListeners();
 
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Моя первая программа на JavaFX, не бейте");
+        primaryStage.setScene(new Scene(root, 700, 500));
         primaryStage.show();
     }
 
@@ -57,14 +74,58 @@ public class Main extends Application {
             @Override
             public void handle(ActionEvent actionEvent) {
                 fileChooser.setTitle("Выбор файла");
-                File selectedFile = fileChooser.showOpenDialog(null);
+                selectedFile = fileChooser.showOpenDialog(null);
                 if (selectedFile != null) {
                     System.out.println(selectedFile.getAbsolutePath());
-//                    System.out.println(selectedFile.getCanonicalPath());
-                    System.out.println(selectedFile.getPath());
                 }
-                else
+                else {
                     System.out.println("file is not valid");
+                    return;
+                }
+                if (horizBox.getChildren().size() > 1){
+                    horizBox.getChildren().remove(1);
+                }
+                try {
+                    image = ImageIO.read(selectedFile);
+                    ImageView iv = new ImageView();
+                    iv.setImage(SwingFXUtils.toFXImage(image, null));
+                    horizBox.getChildren().add(iv);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        btnDoCode.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if (image == null) {
+                    return 0;
+                }
+                int pixelBuffer[] = new int[image.getWidth() * image.getHeight()];
+                for (int y = 0; y < image.getHeight(); y++)
+                    for (int x = 0; x < image.getWidth(); x++)
+                        pixelBuffer[y * image.getWidth() + x] = image.getRGB(x,y);
+                // here is the func
+
+                for (int y = 0; y < image.getHeight(); y++)
+                    for (int x = 0; x < image.getWidth(); x++)
+                        image.setRGB(x,y,pixelBuffer[y * image.getWidth() + x]);
+
+                horizBox.getChildren().remove(1);
+                ImageView iv = new ImageView();
+                iv.setImage(SwingFXUtils.toFXImage(image, null));
+                horizBox.getChildren().add(iv);
+            }
+        });
+
+        btnSaveFile.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+
+
+
+
             }
         });
     }
